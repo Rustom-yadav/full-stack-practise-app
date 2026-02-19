3b472d59e53611c1b6a4a06886d9bcc0a85f46fe
vijay yadav <vijayyadav.official@outlook.com>
2026-02-19 17:09:09 +0530

    server.js modified

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,12 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";        
 dotenv.config();
+const app = express();
 app.use(cors({
   origin: process.env.FRONTEND_URL,
 }));
 
-const app = express();
+
 const PORT = process.env.PORT || 5000;
 
 const jokes = [
